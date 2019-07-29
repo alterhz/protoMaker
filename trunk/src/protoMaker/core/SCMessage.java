1b7cc9feefce44c3b795d5e5203b808a0a36a0a3
hanzhe <550360139@qq.com>
2019-07-29 16:07:40 +0800

    Adjust the directory

--- trunk/src/protoMaker/core/SCMessage.java
+++
@@ -1,9 +0,0 @@
-package protoMaker.core;
-
-public class SCMessage extends Message {
-
-	public SCMessage(int msgId, String baseName) {
-		super(msgId, baseName, "SC" + baseName);
-	}
-
-}
--- trunk/src/protoMaker/core/SCMessageResult.java
+++
@@ -1,9 +0,0 @@
-package protoMaker.core;
-
-public class SCMessageResult extends Message {
-
-	public SCMessageResult(int msgId, String baseName) {
-		super(msgId, baseName, "SC" + baseName + "Result");
-	}
-
-}
